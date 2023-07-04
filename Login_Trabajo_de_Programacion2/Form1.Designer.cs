d44b0415884066c8cb783562e35ca4146ad8d8c1
Daniel <danielrodriguezvexenat@outlook.com>
2023-07-03 22:27:12 -0300

    Agregar archivos de proyecto.

---
+++ Login_Trabajo_de_Programacion2/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Login_Trabajo_de_Programacion2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxTUsuario = new System.Windows.Forms.TextBox();
+            this.TxtContraseña = new System.Windows.Forms.TextBox();
+            this.BtnEntrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Usuario";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Contraseña";
+            // 
+            // TxTUsuario
+            // 
+            this.TxTUsuario.Location = new System.Drawing.Point(159, 68);
+            this.TxTUsuario.Name = "TxTUsuario";
+            this.TxTUsuario.Size = new System.Drawing.Size(100, 20);
+            this.TxTUsuario.TabIndex = 2;
+            // 
+            // TxtContraseña
+            // 
+            this.TxtContraseña.Location = new System.Drawing.Point(159, 132);
+            this.TxtContraseña.Name = "TxtContraseña";
+            this.TxtContraseña.Size = new System.Drawing.Size(100, 20);
+            this.TxtContraseña.TabIndex = 3;
+            // 
+            // BtnEntrar
+            // 
+            this.BtnEntrar.Location = new System.Drawing.Point(110, 209);
+            this.BtnEntrar.Name = "BtnEntrar";
+            this.BtnEntrar.Size = new System.Drawing.Size(75, 23);
+            this.BtnEntrar.TabIndex = 4;
+            this.BtnEntrar.Text = "Entrar";
+            this.BtnEntrar.UseVisualStyleBackColor = true;
+            this.BtnEntrar.Click += new System.EventHandler(this.BtnEntrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(298, 373);
+            this.Controls.Add(this.BtnEntrar);
+            this.Controls.Add(this.TxtContraseña);
+            this.Controls.Add(this.TxTUsuario);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxTUsuario;
+        private System.Windows.Forms.TextBox TxtContraseña;
+        private System.Windows.Forms.Button BtnEntrar;
+    }
+}
+
---
+++ Login_Trabajo_de_Programacion2/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Linq.Expressions;
+
+
+namespace Login_Trabajo_de_Programacion2
+{
+    public partial class Form1 : Form
+    {
+        OleDbConnection conexion = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=D:\\Escritorio\\TP\\BD\\Login_Usuario.mdb");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                conexion.Open();
+                MessageBox.Show("Conectado");
+            }
+            
+            catch (Exception a)
+                {
+                    MessageBox.Show("Error por: " + a.ToString());
+                
+            }
+            
+
+        }
+
+        private void BtnEntrar_Click(object sender, EventArgs e)
+        {
+            string consulta = "Escriba su contraseña = " + TxtContraseña.Text + "Escriba su usario = " + TxTUsuario + " ;";
+            OleDbCommand comando = new OleDbCommand(consulta, conexion);
+            OleDbDataReader leeBd;
+            leeBd = comando.ExecuteReader();
+            Boolean existereg = leeBd.HasRows;
+            if (existereg )
+            {
+                MessageBox.Show("Bienvenido al Sistema " + TxTUsuario);
+                Form2 abrir = new Form2();
+                this.Hide();
+                abrir.Show();
+            }
+            else
+            {
+                MessageBox.Show("Usuario o contraseña incorrecto, ingrese de nuevo");
+                return;
+            }
+            conexion.Close();
+   
+        }
+    }
+}
